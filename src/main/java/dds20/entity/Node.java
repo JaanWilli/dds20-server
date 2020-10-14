5313e3a1b46e320de868b8ddfc9e79708bfd152f
JaanWilli <jan.willi@uzh.ch>
2020-10-14 14:11:01 +0200

    Node: added getter/setter for id

--- src/main/java/dds20/entity/Node.java
+++ src/main/java/dds20/entity/Node.java
@@ -26,6 +26,14 @@ public class Node implements Serializable {
     @Column(nullable = false)
     private String dieAfter;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Boolean getActive() { return active; }
 
     public void setActive(Boolean active) { this.active = active; }
