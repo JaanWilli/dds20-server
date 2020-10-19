560b483f0b82194a343d846c04df2fdd3d962e90
regloff <remy.egloff@flashcable.ch>
2020-10-19 13:06:24 +0200

    Fixed SQL bug

--- src/main/java/dds20/entity/Node.java
+++ src/main/java/dds20/entity/Node.java
@@ -36,7 +36,7 @@ public class Node implements Serializable {
     @Column
     private String dieAfter;
 
-    @Column(nullable = false)
+    @Column(nullable = true) //TODO: Change this again after api specifications changed
     private String node;
 
     @Column(nullable = false)
